53c35c11c4a2e94c85af252ddee3af0fc8044bf6
leonardo <leonardothomazellif@gmail.com>
2026-03-27 22:17:24 -0300

    added timeout to resend email confirmation and refactoring product delete scenarios

--- TSI.Friday.UIApp/src/app/products/components/product-form/product-form.component.ts
+++ TSI.Friday.UIApp/src/app/products/components/product-form/product-form.component.ts
@@ -126,6 +126,61 @@ export class ProductFormComponent
     }
   }
 
+  delete(): void {
+    this.modalService
+      .showSweetConfirmation(
+        '',
+        'Deseja realmente excluir este registro?',
+        'question',
+      )
+      .then((result: any) => {
+        if (result.isConfirmed) {
+          this.productService
+            .delete(this.data as Product)
+            .pipe(
+              tap({
+                next: (response: WebApiResponse<Product>) => {
+                  if (this.isModal) {
+                    this.modalService.hideModal(this.dialogRef);
+                    this.modalService.showSweetNotification(
+                      response.status === ResponseStatus.Success
+                        ? 'Produto excluído'
+                        : 'Erro ao excluir produto',
+                      response.message,
+                      response.status === ResponseStatus.Success
+                        ? 'success'
+                        : 'error',
+                    );
+                  } else {
+                    this.modalService.showSweetNotification(
+                      response.status === ResponseStatus.Success
+                        ? 'Produto excluído'
+                        : 'Erro ao excluir produto',
+                      response.message,
+                      response.status === ResponseStatus.Success
+                        ? 'success'
+                        : 'error',
+                    );
+                    if (response.status === ResponseStatus.Success) {
+                      this.routerService.navigateByUrl(
+                        `/${this._baseEndPoint}`,
+                      );
+                    }
+                  }
+                },
+                error: (err) => {
+                  this.notificationService.showMessage(
+                    'error',
+                    'Erro ao remover',
+                  );
+                },
+              }),
+            )
+            .subscribe();
+        }
+      });
+  }
+
   onPriceBlur(): void {
     const priceControl = this.form.get('priceFormatted');
     if (!priceControl) {
