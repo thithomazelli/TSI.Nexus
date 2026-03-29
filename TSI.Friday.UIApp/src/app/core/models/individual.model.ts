2a59526fdf338187bcba57b22abbf850e75967b0
Thiago Thomazelli Ferreira <thiago.thomazelli@gmail.com>
2026-03-29 19:31:14 -0300

    Fixing Add Business partner with address

--- TSI.Friday.UIApp/src/app/core/models/individual.model.ts
+++ TSI.Friday.UIApp/src/app/core/models/individual.model.ts
@@ -2,5 +2,5 @@ import { BusinessPartner } from './business-partner.model';
 
 export interface Individual extends BusinessPartner {
   nationalIdCard?: string | null;
-  birthday: Date;
+  birthday?: Date;
 }
